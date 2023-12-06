c027c21164cf1e0e73c5c413c3e93f82707688b0
woogi-on_and_on <143869354+heyfuxkingcheez@users.noreply.github.com>
2023-12-06 09:52:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for(let i = left; i<=right; i++) {
+        let arr = [];
+       for(let j = 0; j <= i; j++) {
+           if(i % j === 0) arr.push(j)
+       }
+        //console.log(arr.length)
+       answer += arr.length % 2 === 0 ? i : -i;
+    }
+    return answer;
+}
