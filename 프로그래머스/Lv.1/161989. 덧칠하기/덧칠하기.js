98c230e69f324e22a8a998ed01469b146457797c
woogi-on_and_on <143869354+heyfuxkingcheez@users.noreply.github.com>
2024-01-15 13:58:36 +0900

    [level Lv.1] Title: 덧칠하기, Time: 1.40 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,12 @@
+function solution(n, m, section) {
+    let tried = 0
+    let maxRange = -Infinity
+ 
+    section.forEach(range=>{
+        if(maxRange < range){
+            tried++
+            maxRange = range+m-1
+        }
+    })
+    return tried
+}
