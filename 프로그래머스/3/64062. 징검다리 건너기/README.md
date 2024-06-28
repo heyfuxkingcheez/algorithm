0643862f35ff8a3ddb72eca2e46675aec579a3d9
woogi-on_and_on <143869354+heyfuxkingcheez@users.noreply.github.com>
2024-06-28 23:46:59 +0900

    [level 3] Title: 징검다리 건너기, Time: 12.50 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=javascript) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 23:46:40
+2024년 06월 28일 23:46:57
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,21 +1,28 @@
 function solution(stones, k) {
-    let left = 1;
-    let right = 200000000;
-
-    while(left <= right) {
-        const mid = (left + right) / 2 >> 0;
-
-        let count = 0;
-        for(let i = 0; i < stones.length; i++) {
-            if(stones[i] - mid <= 0) count++;
-            else count = 0;
-
-            if(count === k) break;
+    let answer = 0;
+    let index = 0;
+    let contiguousArr = [];
+    let asdMap = new Map()
+    
+    
+    
+    while (index < stones.length - 2){
+        if(index < stones.length){
+            if(stones[index] > 0) {
+                stones[index]--;
+                index++;
+            }else {
+                console.log(stones[index])
+                asdMap.set(index, stones[index])
+                index++;
+            }
+        }else { 
+            index = 0;
+            answer++;
         }
-
-        if(count === k) right = mid - 1;
-        else left = mid + 1;
+        
     }
-
-    return left;
+    console.log(stones)
+   
+    return answer
 }
